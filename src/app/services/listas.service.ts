5f89f6203791247a416464ab158c3e6ccf6cb4d2
Pa7ry <34232599+Pa7ry@users.noreply.github.com>
2018-06-18 13:05:16 +0200

    service extended

--- src/app/services/listas.service.ts
+++ src/app/services/listas.service.ts
@@ -5,13 +5,26 @@ import { Lista } from './../clases/lista';
 export class ListasService{
 
     listas: Lista[] = [];
+	
+	Lista: ListaItem[] = []; 
 
     constructor() {
 
         let lista1 = new Lista('Planes de novios');
-        let lista2 = new Lista('Mudanza');
-
-        this.listas.push( lista1 );
+		this.lista1.push(new ListaItem ('Ir de viaje', false));
+		this.lista1.push(new ListaItem ('Hacernos fotos', true));
+		this.lista1.push(new ListaItem ('Comer en el Pirata's', false));
+		this.lista1.push(new ListaItem ('Probar supersimuladores', false));
+		
+		 this.listas.push( lista1 );
+		
+        let lista2 = new Lista('Mudanza', true, false, 'ios-home' );
+		this.lista2.push(new ListaItem('Ir a Ikea', true));
+		this.lista2.push(new ListaItem('LLevar cockpit', true));
+		this.lista2.push(new ListaItem('Comprar mesa', true));
+		this.lista2.push(new ListaItem('Llevar ropa', true));
+		this.lista2.push(new ListaItem('Reorganizar habitación', true));
+		
         this.listas.push( lista2 );
 
         console.log('Service Ready')
